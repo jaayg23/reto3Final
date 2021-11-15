52f9e3cf5c6f0f84dc13e4501e9522db112d90de
= <jaaygi01@gmail.com>
2021-11-15 15:45:46 -0500

    Inicializamos el proyecto

---
+++ src/main/java/Reto3Final/Reto3FinalApplication.java
@@ -0,0 +1,13 @@
+package Reto3Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Reto3Final/Reto3FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto3Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
